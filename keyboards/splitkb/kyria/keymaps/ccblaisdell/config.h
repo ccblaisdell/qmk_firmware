7c2250a3771a7717828b0bfa42ea445ea7830bdf
Colby Blaisdell <ccblaisdell@gmail.com>
2024-01-12 13:17:05 -0600

    Add caps words and window switcher

--- keyboards/splitkb/kyria/keymaps/ccblaisdell/config.h
+++ keyboards/splitkb/kyria/keymaps/ccblaisdell/config.h
@@ -47,6 +47,6 @@
 #define MOUSEKEY_WHEEL_MAX_SPEED   8
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 40
 
-#define COMBO_TERM 25        // how quickly all combo keys must be pressed in succession to trigger
+#define COMBO_TERM 50        // how quickly all combo keys must be pressed in succession to trigger
 #define COMBO_MUST_HOLD_MODS // if a combo triggers a modifier, only trigger when the combo is held
-#define COMBO_HOLD_TERM 175  // how long at least one of the combo keys must be held to trigger
+#define COMBO_HOLD_TERM 0  // how long at least one of the combo keys must be held to trigger
