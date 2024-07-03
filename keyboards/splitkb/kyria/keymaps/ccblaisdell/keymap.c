7e11e9f5e447a2bbf1d28d6567a4fb6d602188cc
Colby Blaisdell <ccblaisdell@gmail.com>
2024-07-03 14:39:04 -0500

    Update keymap.c
    Duplicate upper pinky keys to the outside so we can try out a triangular pinky cluster

--- keyboards/splitkb/kyria/keymaps/ccblaisdell/keymap.c
+++ keyboards/splitkb/kyria/keymaps/ccblaisdell/keymap.c
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-------------------------------------------.                              ,-------------------------------------------.
  * |  Tab   |   Q  |   W  |   E  |   R  |   T  |                              |   Y  |   U  |   I  |   O  |   P  |  Bksp  |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |Ctrl/Esc|   A  |   S  |   D  |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |  '  "  |
+ * |    Q   |   A  |   S  |   D  |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |   P    |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |LShift|LShift|  | Fkeys|RShift|   N  |   M  | ,  < | . >  | /  ? | RShift |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
@@ -86,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [_QWERTY] = LAYOUT(
      KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                          KC_Y,   KC_U ,     KC_I ,   KC_O ,  KC_P , KC_BSPC,
-     KC_ESC  , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                          KC_H,   KC_J ,     KC_K ,   KC_L ,KC_SCLN,KC_QUOTE,
+     KC_Q    , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                          KC_H,   KC_J ,     KC_K ,   KC_L ,KC_SCLN, KC_P,
      KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LSFT, KC_LSFT ,  FKEYS  , KC_RSFT,    KC_N,   KC_M ,   KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
                                 KC_LCTL, KC_LCTL, KC_LGUI, KC_LSFT, ESC_SYM ,  ENT_NAV,  KC_SPC, GUI_TAB, KC_LALT,   KC_RALT
     ),
